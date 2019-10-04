7149634e98bbd94d56f4a6fd93dc8b1723f8365e
alex-doerfler <a.doerfler@domus-software.de>
2019-10-04 08:06:04 +0200

    initial commit

---
+++ src/OpenSpace2019Terraform.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OpenSpace2019Terraform.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
